292a31d81f58aee87883db6d790c729e851f4262
eation <eationchou1018@foxmail.com>
2018-08-06 14:56:12 +0800

    test change

--- CountTest2.java
+++ CountTest2.java
@@ -1,14 +1,14 @@
-public class CountTest{
+public class CountTest2{
 	public static void main(String []args){
 		for(int i =0;i<60;i++){
-			System.out.print('test-');
+			System.out.print("test-");
 			System.out.println(i*2+1);
 			try{
-				Thread.sleep(5000);
+				Thread.sleep(150);
 			}catch(Exception e){
 				System.exit(0);
 			}
 		}
-		System.out.println("Task was successed!")
+		System.out.println("Task was successed!");
 	}
 }
