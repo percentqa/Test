4db739b9e12ba58c1096ed0c4999c6f88971dfa0
eation <399567553@qq.com>
2018-08-06 09:55:15 +0800

    test2

--- CountTest2.java
+++ CountTest2.java
@@ -2,12 +2,13 @@ public class CountTest{
 	public static void main(String []args){
 		for(int i =0;i<60;i++){
 			System.out.print('test-');
-			System.out.println(i+1);
+			System.out.println(i*2+1);
 			try{
-				Thread.sleep(1000);
+				Thread.sleep(5000);
 			}catch(Exception e){
 				System.exit(0);
 			}
 		}
+		System.out.println("Task was successed!")
 	}
 }
