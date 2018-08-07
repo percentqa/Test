a619ab5c9244df9d165b1a9788f03397191ba868
yuxuan.zhou <yuxuan.zhou@percent.cn>
2018-08-07 14:53:26 +0800

    test jenkins continue!

--- CountTest2.java
+++ CountTest2.java
@@ -2,7 +2,7 @@ public class CountTest2{
 	public static void main(String []args){
 		for(int i =0;i<70;i++){
 			System.out.print("test-");
-			System.out.println(i*4);
+			System.out.println(i*5);
 			try{
 				Thread.sleep(300);
 			}catch(Exception e){
